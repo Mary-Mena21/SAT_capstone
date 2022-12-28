07add41364c6a5e0cf5b1196f2f2cd8e9be0ec16
Mariana Mena <maranashokry@ahoo.com>
2022-12-28 00:48:57 -0600

    AWS Amplify install

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+import { Amplify} from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
