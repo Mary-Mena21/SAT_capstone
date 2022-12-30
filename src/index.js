b30bd721f02a0508e8b93402f71c25af9befac75
Mariana Mena <maranashokry@ahoo.com>
2022-12-29 22:21:19 -0600

    fix merger conflict

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-import { Amplify} from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+
 
 
 const container = document.getElementById("root");
